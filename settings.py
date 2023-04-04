2b4d356aa573280b8483479c50345a68868f0c5d
danielp375 <118190856+danielp375@users.noreply.github.com>
2023-04-04 07:14:12 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
-MODEL_NAME = "res/best_model_90806.pth"
-CONFIG_PATH = "res/config.json"
-OUTPUT_FILE = "res/output.wav"
+MODEL_NAME = "best_model_90806.pth"
+CONFIG_PATH = "config.json"
+OUTPUT_FILE = "output.wav"
 PROJECT_ID = "stalwart-motif-379704"
 LANGUAGE_CODE = 'en-US'
 SESSION_ID = 'me'
-CREDENTIALS = 'res/credential.json'
+CREDENTIALS = 'credential.json'
